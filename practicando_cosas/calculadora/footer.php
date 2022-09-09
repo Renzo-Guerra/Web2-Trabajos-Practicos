c40362de143ffe344107835f6cc31cf59c7ad74d
Renzo-Guerra <renzoguerralatt@gmail.com>
2022-09-08 23:35:46 -0300

    🔥 DELETE: Repeated files
    - Se elimino el footer.php y header.php que estaban en calculadora.

--- practicando_cosas/calculadora/footer.php
+++
@@ -1,9 +0,0 @@
-  <footer>
-      
-  </footer>
-  <!-- Importacion del js de bootstrap -->
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
-  <!-- Importacion del js porpio -->
-  <script src="calcular.js"></script>
-</body>
-</html>
